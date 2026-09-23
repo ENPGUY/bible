7d54e64f423603de75d9e9ce7fb6b723b79cc27b
ENPGUY <152174869+ENPGUY@users.noreply.github.com>
2026-09-23 20:47:55 +0900

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,21 @@
+async function signup() {
 
+    const email =
+        document.getElementById("email").value;
+
+    const password =
+        document.getElementById("password").value;
+
+    const { data, error } =
+        await db.auth.signUp({
+            email,
+            password
+        });
+
+    if (error) {
+        alert(error.message);
+        return;
+    }
+
+    alert("회원가입이 완료되었습니다.");
+}
