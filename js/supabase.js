6280f742356f52696f59cd41607cbb9f6911ced9
ENPGUY <152174869+ENPGUY@users.noreply.github.com>
2026-09-23 20:46:30 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_KEY = "YOUR_PUBLISHABLE_KEY";
 
+const { createClient } = supabase;
+
+const db = createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
