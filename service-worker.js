d8e535f8207fb3f56696f8d3175e12631f07ff58
ENPGUY <152174869+ENPGUY@users.noreply.github.com>
2026-09-23 20:38:00 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
+const CACHE_NAME = "bible-app-v1";
 
+const urlsToCache = [
+  "/bible/",
+  "/bible/index.html",
+  "/bible/manifest.json",
+  "/bible/icon-192.png",
+  "/bible/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
