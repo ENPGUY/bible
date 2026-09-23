4af17c6be3dd4d828b562e4a117534323eac3bf9
ENPGUY <152174869+ENPGUY@users.noreply.github.com>
2026-09-23 20:49:08 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,3 +20,29 @@ function showRandomVerse() {
 }
 
 loadVerses();
+
+async function addFavorite(verseId) {
+
+    const {
+        data: { user }
+    } = await db.auth.getUser();
+
+    if (!user) {
+        alert("로그인이 필요합니다.");
+        return;
+    }
+
+    const { error } = await db
+        .from("favorites")
+        .insert({
+            user_id: user.id,
+            verse_id: verseId
+        });
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    alert("말씀을 저장했습니다.");
+}
